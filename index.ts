a5eed765496e8c16d3ab0c612f35248bbc433e65
Anwesh43 <anweshthecool0@gmail.com>
2020-11-17 00:26:33 +0530

    adding RightAngleSquareFill class

--- index.ts
+++ index.ts
@@ -187,3 +187,26 @@ class RASFNode {
         return this 
     }
 }
+
+class RightAngleSquareFill {
+    
+    curr : RASFNode = new RASFNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
