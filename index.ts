ccfa81d862702d34003529b18665faaa2e93f3f2
Anwesh43 <anweshthecool0@gmail.com>
2020-11-17 00:24:34 +0530

    adding RASFNode class

--- index.ts
+++ index.ts
@@ -145,3 +145,45 @@ class Animator {
         }
     }
 }
+
+class RASFNode {
+
+    next : RASFNode 
+    prev : RASFNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new RASFNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawRASFNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : RASFNode {
+        var curr : RASFNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
